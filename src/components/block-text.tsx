219a0f1fc9891d4384f35e4105cd30bc0004d93e
topsinoty <topsinoty@gmail.com>
2026-01-18 17:07:09 +0200

    refector to make block-text acc work

---
+++ src/components/block-text.tsx
@@ -0,0 +1,17 @@
+import { FC, HTMLAttributes } from "react";
+
+export interface BlockTextProps {
+  children: string;
+  options?: HTMLAttributes<HTMLSpanElement> & {
+    text?: HTMLAttributes<HTMLSpanElement>;
+  };
+  className?: HTMLAttributes<HTMLDivElement>["className"];
+}
+
+export const BlockText: FC<BlockTextProps> = ({ options, children }) => (
+  <div className="flex gap-1 items-center group/block-text text-muted justify-between select-none">
+    <span {...options}>{"["}</span>
+    <span {...options?.text}>{children}</span>
+    <span {...options}>{"]"}</span>
+  </div>
+);
