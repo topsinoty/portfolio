ed8c98ac33629eaf9a20e34fb157052889ed7da5
topsinoty <topsinoty@gmail.com>
2026-01-27 16:07:18 +0200

    move projects to it's own file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,98 +1,12 @@
 import { CurrentFocus } from "~/components/cuurent-focus";
 import { Hero } from "~/components/hero";
-import { Project, ProjectCard } from "~/components/project-card";
+import { ProjectCard } from "~/components/project-card";
 import { TechStack } from "~/components/tech-stack";
 import { SpotifyWidget } from "~/components/spotify-widget";
 import { BlockText } from "~/components/block-text";
 import Link from "next/link";
 import { TimeWidget } from "~/components/time-widget";
-
-const PROJECTS: Project[] = [
-  {
-    title: "Flave",
-    slug: "flave",
-    image: "https://picsum.photos/seed/lapikud/1200/800",
-    brief: {
-      id: "flave.ee",
-      name: "Flave",
-      description:
-        "MVP of a finals project. Was supposed to be a recipe search engine. Users can search for recipes based on ingredients they have available.",
-      techStack: [
-        "Next.js",
-        "Typescript",
-        "MongoDB",
-        "Tailwind",
-        "Express.js",
-        "C++",
-        "Figma",
-      ],
-      role: "Lead Frontend Engineer, Devops",
-    },
-    reflection: {
-      content:
-        "Mobile-first is the most important thing for a consumer product. \n\nAuthentication is different from authorization!!",
-      type: "good",
-    },
-    source: {
-      last_updated: null,
-      repo_url: "https://github.com/topsinoty/flave-ee",
-      demo_url: "https://flave.ee",
-    },
-  },
-  {
-    title: "Remondikohvik info site",
-    slug: "remondikohvik",
-    image: "https://picsum.photos/seed/lapikud/1200/800",
-    brief: {
-      id: "remondikohvik.lapikud.ee",
-      name: "remondikohvik",
-      description:
-        "Info page for remondikohvik. An event hosted annually by Lapikud MTÜ. I was one of the major contributors.",
-      techStack: ["Next.js", "Typescript", "Tailwind", "Figma"],
-      role: "Frontend Engineer",
-    },
-    reflection: {
-      content:
-        "Sometimes you may not need a framework. Simple HTML/CSS sometimes is the answer.",
-      type: "good",
-    },
-    source: {
-      repo_url: "https://github.com/Lapikud/remondikohvik",
-      demo_url: "https://remondikohvik.lapikud.ee",
-      last_updated: "12-01-26",
-    },
-  },
-  {
-    title: "Alinea",
-    slug: "alinea",
-    image: "https://picsum.photos/seed/lapikud/1200/800",
-    brief: {
-      id: "alinea_hackathon",
-      name: "Alinea",
-      description:
-        "AI-powered application for analyzing workforce, structure, SWOT and other factors for a startup idea.",
-      techStack: [
-        "Next.js",
-        "Typescript",
-        "Tailwind",
-        "Postgres",
-        "Prisma",
-        "Ollama - Llama 3.2:3b",
-      ],
-      role: "Frontend Engineer, Project Lead",
-    },
-    reflection: {
-      content:
-        "For quick prototyping, don't be ashamed to use AI. AI might not be creative but it is statistical.",
-      type: "kinda bad",
-    },
-    source: {
-      last_updated: null,
-      repo_url: null,
-      demo_url: null,
-    },
-  },
-];
+import { PROJECTS } from "./PROJECTS";
 
 export default function Home() {
   return (
