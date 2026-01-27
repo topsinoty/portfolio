5c5735fc49f7e9d51062980018753dde3146272c
topsinoty <topsinoty@gmail.com>
2026-01-27 17:25:28 +0200

    used text-muted-fg in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,18 +1,27 @@
 import Link from "next/link";
 
 export const Footer = () => (
-  <footer className="py-20 items-center justify-center flex flex-col mt-20 border-t border-white/5 text-center font-mono text-xs text-zinc-600">
+  <footer className="py-20 items-center justify-center flex flex-col mt-20 border-t border-white/5 text-center font-mono text-xs text-muted">
     <div className="mb-4 gap-2 flex">
-      <Link href="/github" className="hover:text-zinc-400 transition-colors">
+      <Link
+        href="/github"
+        className="hover:text-muted-foreground transition-colors"
+      >
         github
       </Link>
-      <Link href="/linkedin" className="hover:text-zinc-400 transition-colors">
+      <Link
+        href="/linkedin"
+        className="hover:text-muted-foreground transition-colors"
+      >
         linkedin
       </Link>
-      <Link href="/mail" className="hover:text-zinc-400 transition-colors">
+      <Link
+        href="/mail"
+        className="hover:text-muted-foreground transition-colors"
+      >
         mail
       </Link>
     </div>
-    <div>&copy; {new Date().getFullYear()} Made w :3 by Topsinoty</div>
+    <div>&copy; 2026 Made w :3 by Topsinoty</div>
   </footer>
 );
