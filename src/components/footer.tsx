d130aa3e090ed8c15f93b1fe6a60d39eeb71d1bd
topsinoty <topsinoty@gmail.com>
2026-01-27 17:29:14 +0200

    fixed links

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,19 +4,25 @@ export const Footer = () => (
   <footer className="py-20 items-center justify-center flex flex-col mt-20 border-t border-white/5 text-center font-mono text-xs text-muted">
     <div className="mb-4 gap-2 flex">
       <Link
-        href="/github"
+        href="https://github.com/topsinoty"
+        target="_blank"
+        rel="noreferrer noopener"
         className="hover:text-muted-foreground transition-colors"
       >
         github
       </Link>
       <Link
-        href="/linkedin"
+        href="https://linkedin.com/in/promise-temitope/"
+        target="_blank"
+        rel="noreferrer noopener"
         className="hover:text-muted-foreground transition-colors"
       >
         linkedin
       </Link>
       <Link
-        href="/mail"
+        href="mailto:oluwatobilobatemi05@gmail.com"
+        target="_blank"
+        rel="noreferrer noopener"
         className="hover:text-muted-foreground transition-colors"
       >
         mail
